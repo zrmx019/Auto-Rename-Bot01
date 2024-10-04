41b5a3d13bdb2a64091bdcac239c7199d578028a
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-10-04 12:55:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from aiohttp import web
 from route import web_server
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 
 class Bot(Client):
