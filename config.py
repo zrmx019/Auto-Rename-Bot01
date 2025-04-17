191c5c4db618cdfff9badea8cf02f85ee1455d8c
zrmx019 <mxzrx831@gmail.com>
2025-04-17 21:15:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "20793620")
+    API_HASH  = os.environ.get("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7616097579:AAEx1f37PqrPQ8PMldt8Or7AQyVHyMblRZ4") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","madflixbotz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6853851676').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "https://t.me/Weoo_Animes") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002523934652"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
